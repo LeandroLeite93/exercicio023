deb26b06a7870d97b3b4a69524a5a430b192c5a1
LeandroLeite93 <leandro93.git@gmail.com>
2023-11-22 17:38:14 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio023/exercicio023.cpp
@@ -0,0 +1,22 @@
+/*
+exercicio023.cpp
+
+escreva um programa que escreva na tela os numeros de 0 até 10
+
+for(inicialização;teste de repetição;reinicialização)
+{
+
+}
+*/ 
+
+#include <iostream>
+
+int main()
+{
+	
+	for (int i = 0; i <= 10; i++)
+	{
+		printf("%d \n", i);
+	} 
+}
+
